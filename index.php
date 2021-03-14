379759f1d42361c4747b3579fed793f9076990ba
lena <lenamartin@outlook.fr>
2021-03-14 23:04:40 +0100

    finalt

--- index.php
+++ index.php
@@ -2,12 +2,32 @@
 
 var_dump ($_GET);
 
-if ( isset( $_GET['submit'] ) ) {}
-        $nom = $_GET ['nom'];
-        $mail = $_GET ['mail'];
-        $company = $_GET ['company'];
-        $industry = $_GET ['industry'];
-        $msg = $_GET ['msg-contact'];
+$mail = $_GET ['mail'];
+$nom = $_GET ['nom'];
+$company = $_GET ['company'];
+$industry = $_GET ['industry'];
+$msg = $_GET ['msg-contact'];
+
+$sent = mail( $mail, $nom, $company);
+
+if ($sent) {
+    echo"<h1> Thank you<h1> <br> <p>Thanks for your trust, we will contact you soon as possible, <p>";
+}
+else {
+    echo "hmmm, something went wrong.. but what?";
+}
+
+
+/*
+if ( mail ( $mail, "Hello", "Thank you for have taken time to complete the form", "We will contact you as soon as we can", "have a beautiful day") )
+{
+    echo "Thanks you for your answers";
+}
+else
+{
+    echo "Something went wrong :(";
+}
+
 
         echo "<h1>Information du client<h1>";
         echo "Name : " . $nom;
@@ -21,6 +41,19 @@ if ( isset( $_GET['submit'] ) ) {}
         echo "Message : " . $msg;
 
      exit;
-     
+*/
 
-?>
+/*
+$mail = $_GET['mail'];
+
+$sent = mail('you@domain.com', 'Feedback Form Submission', $feedback, $headers);
+
+if ($sent) {
+    echo "votre information sont bien pris en compte! Merci";
+} 
+else {
+    echo "une erreur s'est produite";
+}
+*/
+
+?> 
